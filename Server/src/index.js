c1a47701f16590832ae87ada7a7a24d07911d444
dheeraj-6904 <dheerajmandoliya@gmail.com>
2025-03-03 22:49:03 +0530

    compiler modules added and tested
    jdoodle is used for compiler

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,11 +1,18 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv").config();
+import express from "express";
+import mongoose from "mongoose";
+
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use(express.json())
+
+// Import Routes
+import compilerRoutes from "./routes/compile.route.js"; // ES6
 
 const PORT = process.env.PORT || 5000;
 
@@ -18,5 +25,8 @@ const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+ // Routes
+app.use("/api/compile", compilerRoutes);
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
