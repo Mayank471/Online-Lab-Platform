3136bebdd8dfece8e69e2980f4aedb7d49efe9c2
Kanishk1024 <154776366+Kanishk1024@users.noreply.github.com>
2025-03-23 15:54:24 +0530

    UI/UX updated

--- code-block/src/main.jsx
+++ code-block/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import EditorPage from './pages/EditorPage'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <EditorPage />
   </StrictMode>,
 )
