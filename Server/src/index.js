0e1053aaf26d662911be7d199bbb16e1df6b8a26
dheeraj-6904 <dheerajmandoliya@gmail.com>
2025-04-14 17:35:19 +0530

    path in indes.html fixed

--- Server/src/index.js
+++ Server/src/index.js
@@ -40,9 +40,8 @@ app.use("/api/classroom",classroomRoutes);
 if(process.env.NODE_ENV === "production"){
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend" + "dist" + "index.html"));
-  }
-  )
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
 }
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
